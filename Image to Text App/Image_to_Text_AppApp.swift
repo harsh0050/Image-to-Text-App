aaa1fa9d66379e77c9823c2dca3888bdd7187fc8
Harsh Bhikadiya <harsh.bhikadiya55@gmail.com>
2024-03-18 19:58:28 +0530

    Initial Commit

---
+++ Image to Text App/Image_to_Text_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_to_Text_AppApp.swift
+//  Image to Text App
+//
+//  Created by Harsh Bhikadiya on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Image_to_Text_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
